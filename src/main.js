5f314e7f66dae5a35d6bd736251bea13b5386d63
Mehmet-Kaan <taspunar10@hotmail.com>
2021-12-08 12:59:27 +0100

    update index.html, main.js
    added "defer" to both scripts, changed title name and parent key in configs

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const config = {
     width : 800,
     height : 600,
     renderer : Phaser.AUTO,
-    parent:'my-game',
+    parent:'game',
     state:{
         preload: preload, 
         create: create, 

fdc70c4fc4ebbccac7ea1a958be56be88c4048bd
Mehmet-Kaan <taspunar10@hotmail.com>
2021-12-08 12:31:03 +0100

    Update main.js
    Skapade grundläggande funktionerna för Alpha

---
+++ src/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Skapa Phaser.Game object men namnet "game"
+let game = Phaser.game(config);
+
+// Preload game assets 
+// Här laddas alla assets innan spelet är igång
+function preload() {
+
+}
+
+// Create game world 
+// Sätts igång när preload() är uppladdad
+function create(){
+
+}
+
+// Update gameplay 
+// Uppdaterar spelet var 16 ms
+// Körs kontunierlig efter create() är färdig
+function update(){
+
+}

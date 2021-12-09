a124c933fd841bc50a2ace0ba755e5b150cb9d1f
Mehmet-Kaan <taspunar10@hotmail.com>
2021-12-09 23:56:48 +0100

    completed player_movement

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ const config = {
     height : 600,
     renderer : Phaser.AUTO,
     parent:'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
     scene:{
         preload: preload,
         create: create,
@@ -15,7 +21,7 @@ const config = {
 let game = new Phaser.Game(config);
 
 //Globala variabler
-let aganju;
+let aganju, cursor;
 
 // Preload game assets 
 // Här laddas alla assets innan spelet är igång
@@ -30,32 +36,62 @@ function preload() {
 // Create game world 
 // Sätts igång när preload() är uppladdad
 function create(){
-    //Startar spelet
-    //game.physics.startSystem(Phaser.Physics.ARCADE);
-
 
     //Skapar spelplanen
     this.add.image(0,0,'background').setOrigin(0);
-
-    //Skapar spelets gränser
-    //this.world.setBounds(0, 0, 800, 600);
-    
     
     //Skapar Aganju
-    aganju = this.add.sprite(350, 400,'aganju');
+    aganju = this.physics.add.sprite(350, 400,'aganju');
 
-    //Skapar animationer för Aganju
-    aganju.animations.add('down', [0,1,2,0], 10, true);
-    aganju.animations.add('left', [4,5,6,4], 10, true);
-    aganju.animations.add('right', [7,8,9,7], 10, true);
-    aganju.animations.add('up', [10,11,12,10], 10, true);
+    //Skalar upp Aganju
+    aganju.setScale(2);
+    
+    //Begränsar Aganju inom spelets gränser
+    aganju.setCollideWorldBounds(true);
 
-    //
-    game.physics.arcade.enable(aganju);
+    //Definierar variabeln cursor = down,left,up,right
+    cursor = this.input.keyboard.createCursorKeys();
+
+    //Skapar down animationen för Aganju
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('aganju', { 
+            start: 0, 
+            end: 2
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    //Skapar left animationen för Aganju
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('aganju', { 
+            start: 3, 
+            end: 5
+        }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    //Skapar right animationen för Aganju
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('aganju', { start: 6, end: 8}),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    //Skapar up animationen för Aganju
+    this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('aganju', { start: 9, end: 11}),
+        frameRate: 10,
+        repeat: -1
+    });
     
     //Ger vikt på Aganju
     aganju.body.mass = 2;
-
 }
 
 // Update gameplay 
@@ -63,4 +99,75 @@ function create(){
 // Körs kontunierlig efter create() är färdig
 function update(){
 
+    //Om vänster pillen trycks, 
+    //left animation spelas
+    if (cursor.left.isDown == true)
+    {
+        aganju.setVelocity(-100,0);
+        aganju.anims.play('left', true);
+    }
+
+    //Om höger pillen trycks, 
+    //right animation spelas
+    else if (cursor.right.isDown)
+    {
+        aganju.setVelocity(100,0);
+        aganju.anims.play('right', true);
+    }
+
+    //Om up pillen trycks, 
+    //Up animation spelas
+    else if(cursor.down.isDown)
+    {
+        aganju.setVelocity(0,100);
+        aganju.anims.play('down', true);
+    }
+
+    //Om up pillen trycks, 
+    //Up animation spelas
+    else if(cursor.up.isDown)
+    {
+        aganju.setVelocity(0,-100);
+        aganju.anims.play('up', true);
+    }
+
+    //Annars ingen rörelse
+    //Ingen animation
+    else{
+        aganju.setVelocity(0,0);
+        aganju.anims.stop();
+    }
+
+    //Om left och up pillarna trycks, 
+    //Aganju går diagonalt till vänster hörn
+    if(cursor.left.isDown && cursor.up.isDown){    
+        aganju.setVelocityY(-100);
+        aganju.setVelocityX(-100);
+        aganju.anims.play('up', true);
+    }
+
+    //Om höger och up pillarna trycks, 
+    //Aganju går diagonalt till höger hörn
+    if(cursor.right.isDown && cursor.up.isDown){    
+        aganju.setVelocityY(-100);
+        aganju.setVelocityX(100);
+        aganju.anims.play('up', true);
+    }
+
+    //Om höger och ner pillarna trycks, 
+    //Aganju går diagonalt till höger hörn
+    if(cursor.right.isDown && cursor.down.isDown){    
+        aganju.setVelocityY(100);
+        aganju.setVelocityX(100);
+        aganju.anims.play('down', true);
+    }
+
+    //Om vänster och ner pillarna trycks, 
+    //Aganju går diagonalt till höger hörn
+    if(cursor.left.isDown && cursor.down.isDown){    
+        aganju.setVelocityY(100);
+        aganju.setVelocityX(-100);
+        aganju.anims.play('down', true);
+    }
+  
 }

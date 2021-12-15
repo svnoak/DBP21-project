87e949118389a83c50562330c8f50bd1b400ebc4
melsowole <82158719+melsowole@users.noreply.github.com>
2021-12-15 13:41:08 +0100

    prep

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import LoadingScene from "./scenes/loading.js";
-import MainMenuScene from "./scenes/mainMenu.js";
-import ProfileScene from "./scenes/profile.js";
-import SigninScene from "./scenes/login.js";
-import SignupScene from "./scenes/signup.js";
-import LeaderboardScene from "./scenes/leaderboard.js";
+// import LoadingScene from "./scenes/loading.js";
+// import MainMenuScene from "./scenes/mainMenu.js";
+// import ProfileScene from "./scenes/profile.js";
+// import SigninScene from "./scenes/login.js";
+// import SignupScene from "./scenes/signup.js";
+// import LeaderboardScene from "./scenes/leaderboard.js";
 import GameScene from "./scenes/game.js";
-import PauseScene from "./scenes/pause.js";
-import GameoverScene from "./scenes/gameover.js";
-import VictoryScene from "./scenes/victory.js";
-import HowToScene from "./scenes/howTo.js";
-import AboutScene from "./scenes/about.js";
+// import PauseScene from "./scenes/pause.js";
+// import GameoverScene from "./scenes/gameover.js";
+// import VictoryScene from "./scenes/victory.js";
+// import HowToScene from "./scenes/howTo.js";
+// import AboutScene from "./scenes/about.js";
 
 
 //const för Spels konfiguration
@@ -24,19 +24,20 @@ const config = {
             debug: false
         }
     },
+    pixelArt:true, // gör 
     scene:[
-        LoadingScene,
-        MainMenuScene,
-        ProfileScene,
-        SigninScene,
-        SignupScene,
-        LeaderboardScene,
+        // LoadingScene,
+        // MainMenuScene,
+        // ProfileScene,
+        // SigninScene,
+        // SignupScene,
+        // LeaderboardScene,
         GameScene,
-        PauseScene,
-        GameoverScene,
-        VictoryScene,
-        AboutScene,
-        HowToScene
+        // PauseScene,
+        // GameoverScene,
+        // VictoryScene,
+        // AboutScene,
+        // HowToScene
     ],
 };
 

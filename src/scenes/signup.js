39c34a48fc41c4ae36b553ecc08206a0caf2f92f
Kim Siebeneicher <kimpfimp@posteo.net>
2021-12-20 12:29:51 +0100

    menus depending on logged in status

--- src/scenes/signup.js
+++ src/scenes/signup.js
@@ -4,20 +4,56 @@ class SignupScene extends Phaser.Scene{
     }
 
     create(){
-        let loginBtn = this.add.text(350, 250, "Login");
-        let backBtn = this.add.text(350, 350, "Back to Menu");
+        let signupBtn = this.add.text(500, 550, "Already have an account?");
+        let backBtn = this.add.text(100, 550, "Back to Menu");
 
-        loginBtn.setInteractive();
+        signupBtn.setInteractive();
         backBtn.setInteractive();
 
-        loginBtn.on("pointerdown", () =>{
+        createSignupForm();
+
+        signupBtn.on("pointerdown", () =>{
+            document.querySelector("form").remove();
             this.scene.start("LoginScene");
         });
         
         backBtn.on("pointerdown", () =>{
+            document.querySelector("form").remove();
             this.scene.start("MainMenuScene");
         });
-    }
+    }   
+}
+
+function createSignupForm(){
+    let form = document.createElement("form");
+    let username = document.createElement("input");
+    username.type = "text";
+    username.placeholder = "Username";
+
+    let email = document.createElement("input");
+    email.type = "email";
+    email.placeholder = "Email";
+
+    let password = document.createElement("input");
+    password.type = "password";
+    password.placeholder = "Password";
+
+    let button = document.createElement("button");
+    button.type = "button";
+    button.innerText = "Create account";
+    button.addEventListener("click", () => {
+        signup(username.value, email.value, password.value);
+    });
+
+    form.append(username, email, password, button);
+
+    document.querySelector("#game").append(form);
+}
+
+function signup(username, email, password){
+    console.log(username);
+    console.log(email);
+    console.log(password);
 }
 
 export default SignupScene;

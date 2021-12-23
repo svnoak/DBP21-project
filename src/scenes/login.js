432827e51408d661bead3401d6f7e7e22f202b66
Kim <kimpfimp@posteo.net>
2021-12-23 18:48:54 +0100

    adding login functionality

--- src/scenes/login.js
+++ src/scenes/login.js
@@ -1,28 +1,21 @@
-let userFetch = "../../backend/databas/user.json";
-fetch(userFetch)
-    .then(response => response.json())
-    .then(data => {
-        let {results} = data;
-        results.forEach(user => console.log(user));
-    })
-    .catch(error => console.log(error));
-
-
 class LoginScene extends Phaser.Scene{
     constructor() {
         super('LoginScene');
     }
 
     create(){
-        let loginBtn = this.add.text(500, 550, "Don't have an account?");
+        let loginSuccess;
+        let signupBtn = this.add.text(500, 550, "Don't have an account?");
         let backBtn = this.add.text(100, 550, "Back to Menu");
+        let loginBtn = this.add.text(250, 350, "Login");
 
         createLoginForm();
 
-        loginBtn.setInteractive();
+        signupBtn.setInteractive();
         backBtn.setInteractive();
+        loginBtn.setInteractive();
 
-        loginBtn.on("pointerdown", () =>{
+        signupBtn.on("pointerdown", () =>{
             document.querySelector("form").remove();
             this.scene.start("SignupScene");
         });
@@ -31,41 +24,76 @@ class LoginScene extends Phaser.Scene{
             document.querySelector("form").remove();
             this.scene.start("MainMenuScene");
         });
+
+        loginBtn.on("pointerdown", async () =>{
+            let username = document.querySelector("#username").value;
+            let password = document.querySelector("#password").value;
+
+            await login(username, password);
+            if( sessionStorage["userID"] ){
+                document.querySelector("form").remove();
+                this.scene.start("MainMenuScene");
+            }
+        })
     }
 }
 
 function createLoginForm(){
     let form = document.createElement("form");
-    form.action = "../../backend/login.php";
     form.method = "POST";
 
     let username = document.createElement("input");
     username.type = "text";
     username.name = "username";
     username.placeholder = "Username";
+    username.id = "username";
     username.required;
 
     let password = document.createElement("input");
     password.type = "password";
     password.name = "password";
     password.placeholder = "Password";
+    password.id = "password";
     password.required;
 
-    let button = document.createElement("button");
-    button.type = "button";
-    button.innerText = "Login";
-    button.addEventListener("click", () => {
-        login(username.value, password.value);
-    });
-
-    form.append(username, password, button);
+    form.append(username, password);
 
     document.querySelector("#game").append(form);
 }
 
-function login(username, password){
-    console.log(username);
-    console.log(password);
+async function login(username, password){
+//let userFetch = "../../backend/databas/user.json";
+//let rqst = new Request("/../backend/login.php");
+
+let rqst = new Request("http://localhost:7000/login.php");
+
+let data = {
+    "username": username,
+    "password": password
+}
+
+fetch(rqst, {
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(data)
+})
+    .then( response => {
+        if( response.status === 200 ){
+            return response.json();
+        }else{
+            alert( "Wrong username or password." );
+            return false;
+        }
+    })
+    .then( data => {
+        if( data["userID"] != undefined ){
+            sessionStorage.setItem("userID", data["userID"]);
+            return true;
+        }
+    })
 }
 
 export default LoginScene;

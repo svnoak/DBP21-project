2da666a37aafb136d5f92b85b14acf6cddf373e4
Mehmet-Kaan <taspunar10@hotmail.com>
2022-01-03 18:46:40 +0300

    New game button fixed
    Bug fixed

--- src/scenes/pause.js
+++ src/scenes/pause.js
@@ -41,9 +41,82 @@ class PauseScene extends Phaser.Scene{
             this.scene.stop();
         });
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Restart data
+        //Total coins
+        let totalCoins = 0;
+        //Score
+        let score = 0;
+        //Lives
+        let lives = 3;
+
+        //Regeneration skill learned
+        let regenerationLearned = false;
+        //Regeneration skillen behöver inte cooldownas
+        //För att den har inte använts än
+        let regenerationCoolDown = false;
+        //Level upgrades faktor för regeneration-skill
+        let regenerationCurrentLevelFactor = 10;
+        //Kostnaden för uppgradering
+        let baseCostForRegenerationUpgrade = 100;
+
+        //SpeedBoost skill learned
+        let speedBoostLearned = false;
+        //SpeedBoost skillen behöver inte cooldownas, 
+        //spelaren har inte använt den än
+        let speedCoolDown = false;
+        //Level upgrades faktor för regeneration-skill
+        let speedBoostCurrentLevelFactor = 1;
+        //Kostnaden för uppgradering
+        let baseCostForSpeedBoostUpgrade = 500;
+
+        //Fireball skill learned 
+        let fireballSkillLearned = false;
+        //Fireball skill active
+        let fireballSkillActive = false;
+        let baseCostForFireballLearn = 1000;
+        let baseCostForfireballUpgrade = 100;
+        //Amount fireballs to shoot
+        let amountFireballsToFire = 0;
+
+        //Lightning skill learned
+        let lightningSkillLearned = false;
+        let baseCostForLightningSkill = 1500;
+        let lightningDamage = 50;
+        //If lightning ability in use
+        let lightningSkillActive = false;
+        //Lightning skillen behöver inte cooldownas, 
+        //spelaren har inte använt den än
+        let lightningCoolDown = false;
+
+        this.restartData = {
+            totalCoins,
+            score,
+            lives,
+            regenerationLearned,
+            regenerationCurrentLevelFactor,
+            baseCostForRegenerationUpgrade,
+            regenerationCoolDown,
+            speedBoostLearned,
+            speedCoolDown,
+            speedBoostCurrentLevelFactor,
+            baseCostForSpeedBoostUpgrade,
+            fireballSkillLearned,
+            fireballSkillActive,
+            baseCostForFireballLearn,
+            baseCostForfireballUpgrade,
+            amountFireballsToFire,
+            lightningSkillLearned,
+            lightningDamage,
+            baseCostForLightningSkill,
+            lightningSkillActive,
+            lightningCoolDown
+        }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
         restartBtn.on("pointerdown", ()=>{
             //Restarts game scene
-            this.scene.start("GameScene", this.startData);
+            this.scene.start("GameScene", this.restartData);
         });
 
         upgradeBtn.on("pointerdown", ()=>{

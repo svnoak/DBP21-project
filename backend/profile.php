56f7b5844517d9e8bf4a78e6f1dff09689a4ed61
Kim <kimpfimp@posteo.net>
2022-01-06 13:40:45 +0100

    adding CORS

--- backend/profile.php
+++ backend/profile.php
@@ -1,27 +1,13 @@
 <?php
-//Starts session
-session_start();
-//Fetching utilities.php
-require_once("utilities.php");
+$method = $_SERVER["REQUEST_METHOD"];
 
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-        exit(0);
-    }
+// Den sk. preflight förfrågan ("får jag anropa dig")
+if ($method === "OPTIONS") {
+    // Tillåt alla (origins) och alla headers
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    exit();
+} 
 
 //Checks if the user is logged in by checking if there is a stored ID in session
 if (!isset($_SESSION["id"])) {
@@ -29,10 +15,18 @@ if (!isset($_SESSION["id"])) {
     exit();
 }
 
+//Starts session
+session_start();
+//Fetching utilities.php
+require_once("utilities.php");
+
 //Checks the contenttype
 contentType("application/json");
 //Checks the method
 requestMethod("POST");
+
+// Alla är vällkommna
+header("Access-Control-Allow-Origin: *");
 
 //Saving the ID from sessions in a variable
 $id = $_SESSION["id"];
